8a55a1d035ce4afc6ebbd26f7b76c4165e956576
Saad-Kamran-2006 <saadkamran6ft@gmail.com>
2024-09-22 18:14:09 +0000

    example

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -6,7 +6,12 @@ from app.router.auth import auth_router
 from app.router.user import user_router
 from app.kafka.producer_consumer import kafka_consumer
 from typing import Annotated
-from app.config.setting import KAFKA_USER_REGISTER_TOPIC, KAFKA_CONSUMER_GROUP_ID_FOR_USER_SERVICE, BOOTSTRAP_SERVERS, BOOTSTRAP_SERVER
+from app.config.setting import (
+    KAFKA_USER_REGISTER_TOPIC,
+    KAFKA_CONSUMER_GROUP_ID_FOR_USER_SERVICE,
+    BOOTSTRAP_SERVERS,
+    BOOTSTRAP_SERVER,
+)
 import asyncio
 
 # from app.models.user_model import User, Register_User
@@ -18,7 +23,13 @@ import asyncio
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_tables()
-    new_user = asyncio.create_task(kafka_consumer(KAFKA_USER_REGISTER_TOPIC, BOOTSTRAP_SERVER, KAFKA_CONSUMER_GROUP_ID_FOR_USER_SERVICE))
+    new_user = asyncio.create_task(
+        kafka_consumer(
+            KAFKA_USER_REGISTER_TOPIC,
+            BOOTSTRAP_SERVER,
+            KAFKA_CONSUMER_GROUP_ID_FOR_USER_SERVICE,
+        )
+    )
     # session.add(new_user)
     # session.commit()
     # session.refresh(new_user)
@@ -41,6 +52,7 @@ app: FastAPI = FastAPI(
 
 app.include_router(router=auth_router)
 app.include_router(router=user_router)
+
 
 @app.get("/")
 async def user_service():
